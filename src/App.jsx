d03598b6eeb877fb8251abc7dfa2ee3dd49e2b3b
Swadesh <78480951+Swadesh-Panda@users.noreply.github.com>
2022-10-10 00:26:56 +0530

    feat: added routes for dashboard and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import { ContactsProvider } from "./context/ContactsProvider";
 import { ChatsProvider } from "./context/ChatsProvider";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ContactsProvider>
-        <ChatsProvider>
-          <Dashboard />
-        </ChatsProvider>
-      </ContactsProvider>
-      {/* <Login /> */}
+      <Routes>
+        <Route
+          path="/dashboard"
+          element={
+            <ContactsProvider>
+              <ChatsProvider>
+                <Dashboard />
+              </ChatsProvider>
+            </ContactsProvider>
+          }
+        />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
